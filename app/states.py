59fa35399b9c8ff04be2751e9675c9a6b160f673
Ermek Abdiyev <abdiyevermek6@gmail.com>
2025-12-08 00:14:27 +0500

    feat(states): add FSM states for deadline flow

--- app/states.py
+++ app/states.py
@@ -13,3 +13,10 @@ class CompleteStates(StatesGroup):
 
 class CompanyStates(StatesGroup):
     entering_code = State()
+    creating_name = State()
+    creating_code = State()
+
+
+class ClaimStates(StatesGroup):
+    choosing_deadline = State()
+    entering_custom_deadline = State()
